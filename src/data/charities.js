f00f6411f5433736777615e022202ddd5d352581
abdul-local <abdullah.hamzan@gmail.com>
2020-09-10 13:13:57 +0800

    masih ada di css global div kedeteksi padahal udah di hapus

---
+++ src/data/charities.js
@@ -0,0 +1,18 @@
+module.exports ={
+    charities: [
+        {
+            id:1,
+            title: 'Firt Charity Project ',
+            category:'Money',
+            thumnail:'https://www.flickr.com/photos/mini_malist/9727255983/in/photolist-rNgSFT-GNriYz-fPyJj4-nUYnfx-EiSWz6-4CY69q-28GVJQ4-EuXEmc-PYqiQX-Ea5nec-G2TVsL-2b8sfXH-rmk4MA-Pqba8c-BZzmFW-NyRtxs-2faKBW4-ErpfMa-JAfDJQ-8qtvFc-Ek13oS-ESKvS7-LATLWG-EoR24k-24uv6RY-Ncd8J5-kFyngH-5pGh1N-YCeYSH-XwfU2n-CGPMyw-XkDZpG-GrZuTo-F4VNeS-dGJuC8-yj4Pgs-DYz6eA-XtmYxw-BCTKkp-R4XKvh-KcKQzf-Hnh3vu-FzphfW-Tn8fMQ-HC3SGs-taec2X-DhR6K2-BCYcH5-UxBWjq-QJgueq/',
+            pledged: 0,
+            target: 100000,
+            date_end: +new Date('10 Desember 2020'),
+            profile_photo:'https://www.flickr.com/photos/mini_malist/9727255983/in/photolist-rNgSFT-GNriYz-fPyJj4-nUYnfx-EiSWz6-4CY69q-28GVJQ4-EuXEmc-PYqiQX-Ea5nec-G2TVsL-2b8sfXH-rmk4MA-Pqba8c-BZzmFW-NyRtxs-2faKBW4-ErpfMa-JAfDJQ-8qtvFc-Ek13oS-ESKvS7-LATLWG-EoR24k-24uv6RY-Ncd8J5-kFyngH-5pGh1N-YCeYSH-XwfU2n-CGPMyw-XkDZpG-GrZuTo-F4VNeS-dGJuC8-yj4Pgs-DYz6eA-XtmYxw-BCTKkp-R4XKvh-KcKQzf-Hnh3vu-FzphfW-Tn8fMQ-HC3SGs-taec2X-DhR6K2-BCYcH5-UxBWjq-QJgueq/',
+            profile_name: 'Bardon teron',
+            no_pledges:0,
+
+
+        }
+    ]
+}
---
+++ src/pages/Home.svelte
@@ -0,0 +1,18 @@
+
+<script>
+	// saya mau ambil file dari list
+import CharityList from '../components/CharityList.svelte';
+import Header from '../components/Header.svelte';
+import Footer from '../components/Footer.svelte';
+import Welcome from '../components/Welcome.svelte';
+import Promo from '../components/Promo.svelte';
+// mengambil data dari folder data
+import {charities} from '../data/charities';
+let title='Opendonasi';
+
+</script>
+<Header />
+<Welcome />
+<CharityList {charities} />
+<Promo />
+<Footer />
