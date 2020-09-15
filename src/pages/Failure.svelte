462bd5d0cac9bc09ce009e323bbdc16583410954
abdul-local <abdullah.hamzan@gmail.com>
2020-09-15 10:50:12 +0800

    finished

--- src/pages/Failure.svelte
+++ src/pages/Failure.svelte
@@ -1,4 +1,5 @@
  <!-- popularCauses section -->
+ <!-- djfhdj-->
  <section id="popularcause" class="waypoint-tigger xs-section-padding">
     <div class="container">
         <div class="xs-heading row xs-mb-60 justify-content-center text-center">
