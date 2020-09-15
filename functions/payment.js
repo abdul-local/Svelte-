24dd72e39387ebb0dab834239776a5f9dc85e6c9
abdul-local <abdullah.hamzan@gmail.com>
2020-09-15 12:51:55 +0800

    tette

--- functions/payment.js
+++ functions/payment.js
@@ -1,60 +1,67 @@
-exports.handler = function (event, context, callback){
-    // kita panggil clien midtans
+exports.handler = function (event, context, callback) {
     const Midtrans = require('midtrans-client');
-    const headers ={
+
+    const headers = {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Methods' : 'GET,POST,PUT,DELETE',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
     };
+
     const snap = new Midtrans.Snap({
-        isProudction: true,
-        serverKey:process.env.MIDTRANS_SERVER_KEY,
-        clientKey:process.env.MIDTRANS_CLIENT_KEY,
+        isProduction: true,
+        serverKey: process.env.MIDTRANS_SERVER_KEY,
+        clientKey: process.env.MIDTRANS_CLIENT_KEY
     });
-    const {id, name, email, amount } = JSON.parse(event.body);
+
+    const { id, name, email, amount } = JSON.parse(event.body);
+
     const names = name.split(' ');
     let first_name, last_name;
-    if(names && names.length >1){
+
+    if (names && names.length > 1) {
         first_name = names[0];
         last_name = names[1];
-    }else if (names.length === 1){
-        first_name= names[0];
-        last_name= '';
+    } else if (names.length === 1) {
+        first_name = names[0];
+        last_name = '';
     }
-    const parameters= {
+
+    const parameters = {
         transaction_details: {
-            order_id:`BWA-${id}-${+new Date()}`,
-            gross_amount: parseInt(amount),
+            order_id: `BWACHA-${id}-${+new Date()}`,
+            gross_amount: parseInt(amount)
         },
-        cunstomer_details: {
+        customer_details: {
             first_name,
             last_name,
             email
         },
-        credit_card:{
+        credit_card: {
             secure: true
         }
     }
-    snap.createTransaction(parameters)
-    .then(function(transaction){
-        const {token, redirect_url } = transaction;
-        console.log(`Token: ${token}`);
-        console.log(`Redirect URL: ${redirect_url}`);
 
-        callback(null,{
-            statusCode: 200,
-            headers,
-            body: JSON.stringify({
-                url:redirect_url,
-                params: parameters,
+    snap.createTransaction(parameters)
+        .then(function (transaction) {
+            const { token, redirect_url } = transaction;
+            console.log(`Token: ${token}`);
+            console.log(`Redirect URL: ${redirect_url}`);
+
+            callback(null, {
+                statusCode: 200,
+                headers,
+                body: JSON.stringify({
+                    url: redirect_url,
+                    params: parameters
+                })
+            })
+
+        }).catch(function (err) {
+            console.error(`Error: ${err.message}`);
+            callback(null, {
+                statusCode: 400,
+                headers,
+                body: JSON.stringify({ error: err.message })
             })
         })
-    }).catch(function(err){
-        console.error(`Error: ${err.message}`);
-        callback(null,{
-            statusCode: 400,
-            headers,
-            body:JSON.stringify({error:err.message}),
-        });
-    });
 }
